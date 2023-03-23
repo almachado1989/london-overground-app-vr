eec2690fbd0a199d20ad0b8391f5efa187b8cf3d
almachado1989 <almachado@hotmail.co.uk>
2023-03-23 15:40:05 +0000

    added icon

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -2,6 +2,7 @@ import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
+import { IconButton } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { useTheme } from "@emotion/react"
 
@@ -18,11 +19,12 @@ export default function Nav() {
       }}
     >
       <Toolbar>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1 }}
-        ></Typography>
+        <IconButton onClick={() => navigate("/")} sx={{ mr: 2 }}>
+          <img src="./Underground_(no_text).svg" width={48} />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Route Planner
+        </Typography>
         <Button onClick={() => navigate("/")} color="inherit">
           Home
         </Button>
