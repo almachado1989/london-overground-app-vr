92499bc36be16997013b0aa71ae6d4ee7c42f4e0
almachado1989 <almachado@hotmail.co.uk>
2023-03-24 12:55:08 +0000

    updated images, meta, robots

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -8,8 +8,7 @@ export default function Cards(props) {
       bodyText:
         "One of London's most popular shopping destinations, home to a wide variety of shops and a vibrant atmosphere.",
       buttonText: "Let's go shopping!",
-      image:
-        "https://www.ingka.com/wp-content/uploads/resized/f5/Oxford-Circus-cropped_1200x630_6549094aa7be6f747d89307db9e56a54.jpeg",
+      image: "./images/Oxford-Circus.webp",
       station: "oxford circus",
     },
     {
@@ -17,8 +16,7 @@ export default function Cards(props) {
       bodyText:
         " A modern art museum featuring a vast collection of contemporary and modern art from around the world.",
       buttonText: "Let's get arty!",
-      image:
-        "https://cdn.britannica.com/39/136939-050-4EFA1B8D/Tate-Modern-London.jpg",
+      image: "./images/Tate-Modern-London.webp",
       station: "st. paul's",
     },
     {
@@ -26,8 +24,7 @@ export default function Cards(props) {
       bodyText:
         "An iconic observation wheel offering stunning panoramic views of the city from 135 meters above ground.",
       buttonText: "Let's see the city!",
-      image:
-        "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000,f_auto/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/ikknf4oeysojhyv2z0jk/LondonEyeTickets-FastTrackTicketsAvailable-KlookUnitedKingdom.jpg",
+      image: "./images/LondonEye.webp",
       station: "waterloo",
     },
     {
@@ -35,8 +32,7 @@ export default function Cards(props) {
       bodyText:
         "A world-famous museum with an impressive collection of artifacts from around the world, spanning over two million years of history.",
       buttonText: "Let's see some history!",
-      image:
-        "https://www.britishmuseum.org/sites/default/files/styles/uncropped_large/public/event_default.png?itok=d1iPPzdW",
+      image: "./images/british-museum.webp",
       station: "tottenham court road",
     },
     {
@@ -44,8 +40,7 @@ export default function Cards(props) {
       bodyText:
         "London's entertainment district, home to popular theaters, restaurants, and nightclubs.",
       buttonText: "Let's party!",
-      image:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-528153683-1663764747.jpg?crop=1.00xw:1.00xh;0,0&resize=640:*",
+      image: "./images/west-end.webp",
       station: "leicester square",
     },
     {
@@ -53,7 +48,7 @@ export default function Cards(props) {
       bodyText:
         "A popular shopping destination located in the vibrant Camden neighborhood, with an eclectic mix of shops, stalls, and food vendors.",
       buttonText: "Let's go to Caaamden!",
-      image: "https://media.timeout.com/images/105857286/image.jpg",
+      image: "./images/camden-market.webp",
       station: "camden town",
     },
   ]
